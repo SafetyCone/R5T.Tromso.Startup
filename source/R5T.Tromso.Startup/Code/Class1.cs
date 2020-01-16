b0ab2fba78e03e1069664e4660daa953186f5940
MinexAutomation <minex.automation@gmail.com>
2020-01-16 14:38:29 -0800

    Added UseStartup() IServiceBuilder extension to bind an IStartup instance to an IServiceBuilder instance.

--- source/R5T.Tromso.Startup/Code/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace R5T.Tromso.Startup
-{
-    public class Class1
-    {
-    }
-}
---
+++ source/R5T.Tromso.Startup/Code/Extensions/IServiceBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using R5T.Richmond;
+
+
+namespace R5T.Tromso.Startup
+{
+    public static class IServiceBuilderExtensions
+    {
+        /// <summary>
+        /// Links an <see cref="IStartup"/> instance to an <see cref="IServiceBuilder"/>.
+        /// </summary>
+        public static IServiceBuilder UseStartup(this IServiceBuilder serviceBuilder, IStartup startup)
+        {
+            serviceBuilder.AddConfigureConfiguration(startup.ConfigureConfiguration);
+
+            serviceBuilder.AddConfigureServices(startup.ConfigureServices);
+
+            serviceBuilder.AddConfigure(startup.Configure);
+
+            return serviceBuilder;
+        }
+    }
+}
