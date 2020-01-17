664397218ede04e90bab37c1873b8d3855592d96
MinexAutomation <minex.automation@gmail.com>
2020-01-16 22:54:15 -0800

    Added IBuildableService extensions.

---
+++ source/R5T.Tromso.Startup/Code/Extensions/IBuildableServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using R5T.Richmond;
+using R5T.Tromso.Types;
+
+
+namespace R5T.Tromso.Startup
+{
+    public static class IBuildableServiceExtensions
+    {
+        public static void UseStartup<TStartup>(this IBuildableService buildableService, IServiceProvider configurationServiceProvider)
+            where TStartup : class, IStartup
+        {
+            var serviceBuilder = buildableService.GetServiceBuilder();
+
+            serviceBuilder.UseStartup<TStartup>();
+
+            serviceBuilder.Build(configurationServiceProvider);
+        }
+    }
+}
